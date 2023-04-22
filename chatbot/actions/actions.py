8f638b53da8d3c8778bd3ff7007738875a8bf87c
Ilya Tsakunov <i.tsakunov@student.fontys.nl>
2023-04-23 00:05:34 +0200

    Add GetCanteen action

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,6 +1,8 @@
+import datetime
 from typing import Any, Text, Dict, List
 import requests
 import json
+import pandas as pd
 from pathlib import Path
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import ConversationPaused
@@ -171,5 +173,73 @@ class ActionIntroMessage(Action):
         ]
 
         dispatcher.utter_message(text="Nevíš, jak se zeptat? Tady jsme pro tebe připravili nejčastější okruhy otázek.", buttons=button_resp)
+
+        return []
+
+
+class ActionGetCanteenMenu(Action):
+    def _get_menu(self):
+        headers = {
+            'Accept': 'application/json',
+            'Cookie': 'Anete2=585c60bb-43ac-45d7-804c-df13fd845a0d; vse_cookie_allow=%5B%5D; vse_cookie_set=1',
+            'Referer': 'https://webkredit.vse.cz/webkredit_italska/Ordering/Menu',
+            'Host': 'webkredit.vse.cz',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15',
+            'Accept-Language': 'cs',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+        today = f'{datetime.datetime.now():%Y-%m-%d}'
+        today = '2023-04-24'
+        canteen_menu_url = f'https://webkredit.vse.cz/webkredit_italska/Api/Ordering/Menu?Dates={today}T00%3A00%3A00.000Z&CanteenId=1'
+        try:
+            response = requests.get(canteen_menu_url, headers=headers)
+            menu = response.json()['groups']
+        except:
+            menu = None
+
+        return menu
+
+    def name(self) -> Text:
+        return "action_get_canteen_menu"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        language = tracker.get_slot('langcode')
+
+        todays_menu = self._get_menu()
+
+        menu_df = pd.DataFrame(todays_menu)
+
+        menu_df['mealName'] = menu_df.apply(lambda x: [x['rows'][i]['item']['mealName'] for i in range(len(x['rows']))], axis=1)
+
+        if todays_menu:
+            message = ''
+
+            if language == 'cs':
+                message += 'Dneska v menze mají: <br><br>'
+            else:
+                message += 'Today\'s menu in canteen: <br><br>'
+
+            for _, row in menu_df.iterrows():
+                message += row['mealKindName'] + '<br>'
+                message += ' \u2022 ' + '<br> \u2022 '.join(row['mealName']) + '<br><br>'
+        elif not todays_menu:
+            if language == 'cs':
+                message = 'Jídelníček na dneska není k dispozici'
+            else:
+                message = 'There is no canteen menu for today'
+        else:
+            if language == 'cs':
+                message = 'Momentálně ti bohužel nemůžeme ukázat jídelníček'
+            else:
+                message = 'Unfortunately we could not provide you with menu canteen at the moment'
+
+        print(message)
+        dispatcher.utter_message(text=message)
 
         return []
