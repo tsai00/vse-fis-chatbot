64061d9e020452664e1cc1c8e0aace79611dff10
Ilya Tsakunov <i.tsakunov@student.fontys.nl>
2023-04-22 19:35:10 +0200

    Add langcode entity

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -3,9 +3,18 @@ import requests
 import json
 from pathlib import Path
 from rasa_sdk import Action, Tracker
+from rasa_sdk.events import ConversationPaused
+from rasa_sdk.events import UserUtteranceReverted
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from polyglot.detect import Detector
+
+
+LANGUAGES = {
+    'cs': 'Czech',
+    'en': 'English'
+}
+
 
 class ActionDetectLanguage(Action):
     def name(self) -> Text:
@@ -16,20 +25,26 @@ class ActionDetectLanguage(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         text = tracker.latest_message.get('text')
-        print(text)
+        langcode = tracker.get_slot('langcode')     # Language might be already set (e.g. in payload)
+        print(langcode)
 
-        try:
-            result = Detector(text).language
-            langcode = result.code
-            langname = result.name
-        except:
-            langcode = 'cz'
-            langname = 'Czech'
+        if langcode is None:
+            print(text)
+
+            try:
+                result = Detector(text).language
+                langcode = result.code
+                langname = result.name
+            except:
+                langcode = 'cs'
+                langname = 'Czech'
+        else:
+            langname = LANGUAGES.get(langcode)
 
         if langcode not in ['en', 'cs']:
-            dispatcher.utter_message(text=f'Unfortunately I can not speak {langname}, but can only speak English or Czech')
+            dispatcher.utter_message(
+                text=f'Unfortunately I can not speak {langcode}, but can only speak English or Czech')
 
-        print(langcode)
         print(langname)
 
         return [SlotSet("langcode", langcode), SlotSet("langname", langname)]
@@ -47,7 +62,7 @@ class ActionStudyPrograms(Action):
         language = tracker.get_slot('langcode')
 
         if language not in ['en', 'cs']:
-            language = 'en'
+            language = 'cs'
 
         bachelor_degree = next(tracker.get_latest_entity_values('bachelor_degree'), None)
         master_degree = next(tracker.get_latest_entity_values('master_degree'), None)
@@ -62,28 +77,35 @@ class ActionStudyPrograms(Action):
         print(master_degree)
         print(doctor_degree)
 
+        bachelor_programs = '\n\u2022' + '\n\u2022 '.join(json_dict['bachelor'])
+        master_programs = '\n\u2022' + '\n\u2022 '.join(json_dict['master'])
+        doctor_programs = '\n\u2022' + '\n\u2022 '.join(json_dict['doctor'])
+
+        print(json_dict['master'])
+        print(master_programs)
+
         if all([bachelor_degree is None, master_degree is None, doctor_degree is None]):
             if language == 'cs':
-                message = f"FIS momentálně nabízí následující programy: \nBachelor: {json_dict['bachelor']}\n" + f"Master: {json_dict['master']}\n" + f"PhD: {json_dict['doctor']}\n"
+                message = f"FIS momentálně nabízí následující programy: \nBachelor: {bachelor_programs}\n" + f"Master: {master_programs}\n" + f"PhD: {doctor_programs}\n"
             else:
-                message = f"FIS currently offers following programs: \nBachelor: {json_dict['bachelor']}\n" + f"Master: {json_dict['master']}\n" + f"PhD: {json_dict['doctor']}\n"
+                message = f"FIS currently offers following programs: \nBachelor: {bachelor_programs}\n" + f"Master: {master_programs}\n" + f"PhD: {doctor_programs}\n"
         else:
             message = ''
             if bachelor_degree is not None:
                 if language == 'cs':
-                    message = f'FIS momentálně nabízí následující bakalářské programy:\n {json_dict["bachelor"]}'
+                    message = f'FIS momentálně nabízí následující bakalářské programy:\n {bachelor_programs}'
                 else:
-                    message = f'For Bachelor students FIS currently offers following programs:\n {json_dict["bachelor"]}'
+                    message = f'For Bachelor students FIS currently offers following programs:\n {bachelor_programs}'
             if master_degree is not None:
                 if language == 'cs':
-                    message = f'FIS momentálně nabízí následující magisterské programy:\n {json_dict["master"]}'
+                    message = f'FIS momentálně nabízí následující magisterské programy:\n {master_programs}'
                 else:
-                    message = f'For Master students FIS currently offers following programs:\n {json_dict["master"]}'
+                    message = f'For Master students FIS currently offers following programs:\n {master_programs}'
             if doctor_degree is not None:
                 if language == 'cs':
-                    message = f'FIS momentálně nabízí následující doktorské programy:\n {json_dict["doctor"]}'
+                    message = f'FIS momentálně nabízí následující doktorské programy:\n {doctor_programs}'
                 else:
-                    message = f'For PhD students FIS currently offers following programs:\n {json_dict["doctor"]}'
+                    message = f'For PhD students FIS currently offers following programs:\n {doctor_programs}'
 
         dispatcher.utter_message(text=message)
 
@@ -139,12 +161,13 @@ class ActionIntroMessage(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text="Ahoj, jsem FISBot, virtuální asistent a rád ti poradím. Napíš, co by tě zajímalo. \n"
-                                      "Mluvím česky a anglicky. Můžeš se přepínat mezi jazyky přímo během konverzace.\n")
+                                      "Mluvím česky a anglicky. Můžeš se přepínat mezi jazyky přímo během konverzace.\n"
+                                      "Pamatuj se ale prosím, že se jen učím a nejsem Chat GPT-5 :)")
 
         button_resp = [
-            {"title": "Bakalářské studium", "payload": '/study_programs{"bachelor_degree": ' + f'"bachelor"' + '}'},
-            {"title": "Magisterské studium", "payload": '/study_programs{"master_degree": ' + f'"master"' + '}'},
-            {"title": "Doktorské studium", "payload": '/study_programs{"doctor_degree": ' + f'"doctor"' + '}'},
+            #{"title": "Bakalářské studium", "payload":"/study_programs{/"master_degree/":/"magisterské/"/"slot2/":/"value2/"}},
+            {"title": "Magisterské studium", "payload": '/study_programs{"master_degree": "magisterské", "langcode": "cs"}'},
+            {"title": "Doktorské studium", "payload": '/study_programs{"langcode": ' + f'"cs"' + '}'},
         ]
 
         dispatcher.utter_message(text="Nevíš, jak se zeptat? Tady jsme pro tebe připravili nejčastější okruhy otázek.", buttons=button_resp)
